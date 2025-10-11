cccb18cb3eff2561f2e9ab433e30612f36d23c25
Michal Lurie <Michal.Lurie@gmail.com>
2025-10-11 17:51:05 +0200

    trying to fix icu.cpm

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,33 +1,34 @@
 include(FetchContent)
+
 # Get sqlite3
-FetchContent_Declare(
-    sqlite3
-    GIT_REPOSITORY https://github.com/InCom-0/sqlite3-cmake
-    GIT_TAG v3.50.2
-    EXCLUDE_FROM_ALL
-)
+# FetchContent_Declare(
+#     sqlite3
+#     GIT_REPOSITORY https://github.com/InCom-0/sqlite3-cmake
+#     GIT_TAG v3.50.2
+#     EXCLUDE_FROM_ALL
+# )
     
-FetchContent_MakeAvailable(sqlite3)
+# FetchContent_MakeAvailable(sqlite3)
 
 # Write a fake package config so that subprojects find our SQLite that's defined by sqlite3-cmake
-file(WRITE "${sqlite3_BINARY_DIR}/SQLite3Config.cmake" 
-"if(NOT TARGET SQLite::SQLite3)
-  add_library(SQLite::SQLite3 ALIAS sqlite3)
-endif()")
-set(SQLite3_DIR "${sqlite3_BINARY_DIR}")
+# file(WRITE "${sqlite3_BINARY_DIR}/SQLite3Config.cmake" 
+# "if(NOT TARGET SQLite::SQLite3)
+#   add_library(SQLite::SQLite3 ALIAS sqlite3)
+# endif()")
+# set(SQLite3_DIR "${sqlite3_BINARY_DIR}")
 
 
 # Now bring in sqlpp23
-FetchContent_Declare(
-    sqlpp23
-    GIT_REPOSITORY  https://github.com/rbock/sqlpp23
-    GIT_TAG         0.67
-    EXCLUDE_FROM_ALL
-)
-set(BUILD_SQLITE3_CONNECTOR ON)
-set(CMAKE_FIND_PACKAGE_PREFER_CONFIG ON)
-FetchContent_MakeAvailable(sqlpp23)
-set(CMAKE_FIND_PACKAGE_PREFER_CONFIG OFF)
+# FetchContent_Declare(
+#     sqlpp23
+#     GIT_REPOSITORY  https://github.com/rbock/sqlpp23
+#     GIT_TAG         0.67
+#     EXCLUDE_FROM_ALL
+# )
+# set(BUILD_SQLITE3_CONNECTOR ON)
+# set(CMAKE_FIND_PACKAGE_PREFER_CONFIG ON)
+# FetchContent_MakeAvailable(sqlpp23)
+# set(CMAKE_FIND_PACKAGE_PREFER_CONFIG OFF)
 
 
 ## ICU
@@ -45,9 +46,8 @@ set(CMAKE_FIND_PACKAGE_PREFER_CONFIG OFF)
 set(ICU_NO_INSTALL ON CACHE INTERNAL "")
 FetchContent_Declare(
     icu
-    GIT_REPOSITORY https://github.com/InCom-0/icu-cmake_AiMiDi
-    OVERRIDE_FIND_PACKAGE
-    EXCLUDE_FROM_ALL
+    GIT_REPOSITORY https://github.com/kbinani/icu-cpm
+    GIT_TAG        cpm
 )
 
 FetchContent_MakeAvailable(icu)
